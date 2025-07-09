89734da5348ba60e38bed037cfb582ea3d02b566
刘浩彬 <2353836117@qq.com>
2025-07-10 00:15:03 +0800

    二分查找左闭右开

--- vector/leetcode704.py
+++ vector/leetcode704.py
@@ -1,4 +1,5 @@
-class Solution:
+# 二分查找
+class Solution_1:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)-1
         while(left <= right):
@@ -11,4 +12,16 @@ class Solution:
                 return middle
         return -1
 
-        
+class Solution_2:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) # 定义target在左闭右开的区间里，即：[left, right)
+
+        while(left < right):
+            middle = left + (right - left) // 2
+            if nums[middle] > target:
+                right = middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+        return -1
